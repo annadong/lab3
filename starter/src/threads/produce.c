8c6a83bf257860513cee21dcbea76a39785121dc
Anna Dong <annadong@v1020-wn-101-186.campus-dynamic.uwaterloo.ca>
2018-11-15 16:13:45 -0500

    threading code

--- starter/src/threads/produce.c
+++ starter/src/threads/produce.c
@@ -16,6 +16,17 @@
 
 double g_time[2];
 
+
+int *buffer;
+int *lastProduced;
+int producerSize;
+int bufferSize;
+sem_t spaces;
+sem_t items;
+pthread_mutex_t mutex;
+int pindex = 0;
+int cindex = 0;
+
 int main(int argc, char *argv[])
 {
 	int num;
@@ -25,8 +36,8 @@ int main(int argc, char *argv[])
 	int i;
 	struct timeval tv;
 
-	sem_t empty_list;
-	sem_t full_list;
+	sem_t spaces;
+	sem_t items;
 
 
 	if (argc != 5) {
@@ -34,26 +45,50 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+
 	num = atoi(argv[1]);	/* number of items to produce */
 	maxmsg = atoi(argv[2]); /* buffer size                */
 	num_p = atoi(argv[3]);  /* number of producers        */
 	num_c = atoi(argv[4]);  /* number of consumers        */
 
-	sem_init(&empty_list, 0, maxmsg);
-	sem_init(&full_list, 0, 0);
+	producerSize = num_p;
+	bufferSize = maxmsg;
 
-	pthread_t producers[num_p];
-	pthread_t consumers[num_c];
+	buffer = malloc(maxmsg*sizeof(int));
+	lastProduced = malloc(num_p*sizeof(int));
+
+	for (int i = 0; i < producerSize; i ++) {
+		lastProducer[i] = 0;
+	}
+
+	sem_init(&spaces, 0, maxmsg);
+	sem_init(&items, 0, 0);
+	pthread_mutex_init(&mutex, NULL);
 
 	gettimeofday(&tv, NULL);
 	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
 
-	for (int i = 0; i < num_p; i++) {
-		//sem_wait()
-		pthread_mutex_lock(&mutex);
+	pthread_t threads[num_p+num_c];
 
-		//sem_post()
+	for (int i = 0; i < num_p; i++) {
+		int* id = malloc(sizeof(int));
+		*id = i;
+		pthread_create(&threads[i], NULL, producer, id);
 	}
+
+	for (int j = num_p; j < num_p+num_c; j++) {
+		int* jd = malloc(sizeof(int));
+		*jd = j-num_p;
+		pthread_create(&threads[j], NULL, consumer, jd);
+	}
+
+	for (int k = 0; k < num_p + num_c; k++) {
+		pthread_join(threads[k], NULL);
+	}
+
+	sem_destroy(&spaces);
+	sem_destroy(&items);
+	pthread_mutex_destory(&mutex);
 
 
     gettimeofday(&tv, NULL);
@@ -61,13 +96,20 @@ int main(int argc, char *argv[])
 
     printf("System execution time: %.6lf seconds\n", \
             g_time[1] - g_time[0]);
+	pthread_exit(0);
 	exit(0);
 }
 
 void* producer (void *arg) {
 	int *id = (int *) arg;
-	for(int i = 0l i < 10000; ++i) {
-
+	while (1) {
+		int num = produce(*id);
+		sem_wait(&spaces);
+		pthread_mutex_lock(&mutex);
+		*buffer[pindex] = num;
+		pindex = (pindex + 1) % bufferSize;
+		pthread_mutex_unlock(&mutex);
+		sem_post(&items);
 	}
 	free(arg);
 	pthread_exit(NULL);
@@ -75,9 +117,33 @@ void* producer (void *arg) {
 
 void* consumer(void *arg) {
 	int *id = (int *) arg;
-	for(int i = 0; i < 10000; ++i) {
-
+	while (1) {
+		sem_wait(&items);
+		pthread_mutex_lock(&mutex);
+		int num = buffer[cindex];
+		*buffer[cindex] = -1;
+		cindex = (cindex + 1) % bufferSize;
+		pthread_mutex_unlock(&mutex);
+		sem_post(&spaces);
+		consume(*id, num);
 	}
 	free(arg);
 	pthread_exit(NULL);
+}
+
+int produce(int id) {
+	int num = lastProduced[id];
+	if (num == 0) {
+		lastProduced[id] = id;
+	} else {
+		lastProduced[id] = lastProduced[id] + producerSize;
+	}
+	return lastProduced[id];
+}
+
+int consume(int id, int number) {
+	double squareRoot = sqrt(number);
+	if (squareRoot == (int) squareRoot) {
+		printf(" %d %d %d", id, number, squareRoot)
+	}
 }
