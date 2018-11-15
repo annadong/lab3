7fc3833dcbc868090a008d2a786380e8b4c75cf2
Anna Dong <annadong@v1020-wn-101-186.campus-dynamic.uwaterloo.ca>
2018-11-15 15:47:46 -0500

    started code

--- starter/src/threads/produce.c
+++ starter/src/threads/produce.c
@@ -16,7 +16,6 @@
 
 double g_time[2];
 
-
 int main(int argc, char *argv[])
 {
 	int num;
@@ -25,6 +24,10 @@ int main(int argc, char *argv[])
 	int num_c;
 	int i;
 	struct timeval tv;
+
+	sem_t empty_list;
+	sem_t full_list;
+
 
 	if (argc != 5) {
 		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
@@ -36,9 +39,21 @@ int main(int argc, char *argv[])
 	num_p = atoi(argv[3]);  /* number of producers        */
 	num_c = atoi(argv[4]);  /* number of consumers        */
 
+	sem_init(&empty_list, 0, maxmsg);
+	sem_init(&full_list, 0, 0);
+
+	pthread_t producers[num_p];
+	pthread_t consumers[num_c];
 
 	gettimeofday(&tv, NULL);
 	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
+
+	for (int i = 0; i < num_p; i++) {
+		//sem_wait()
+		pthread_mutex_lock(&mutex);
+
+		//sem_post()
+	}
 
 
     gettimeofday(&tv, NULL);
@@ -47,4 +62,22 @@ int main(int argc, char *argv[])
     printf("System execution time: %.6lf seconds\n", \
             g_time[1] - g_time[0]);
 	exit(0);
+}
+
+void* producer (void *arg) {
+	int *id = (int *) arg;
+	for(int i = 0l i < 10000; ++i) {
+
+	}
+	free(arg);
+	pthread_exit(NULL);
+}
+
+void* consumer(void *arg) {
+	int *id = (int *) arg;
+	for(int i = 0; i < 10000; ++i) {
+
+	}
+	free(arg);
+	pthread_exit(NULL);
 }
